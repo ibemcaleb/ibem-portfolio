d91e8684d62872766af08e5c6f17d38586ca38a4
Caleb Ibem <ibemcaleb@yahoo.com>
2024-05-28 21:23:37 +0100

    Updated About Me section with more details

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,8 +12,8 @@ const Home = () => {
                 <h1>About Me</h1><br />
                 <p>My name is Caleb Ibem, a graduate of Computer Science and a professional web developer.
                   I build functional, dynamic and responsive UI components that scale gracefully on different view devices (mobile, tablet and desktop).
-                  Over the years, I have gathered experiences and mastered the use of various web technologies. 
-                  Such as Reactjs, Redux Toolkit, Nodejs, Express, MongoDB, etc.
+                  Over the years, I have gathered experiences and mastered the use of various web technologies, 
+                  such as JavaScript, TypeScript, Reactjs, Redux Toolkit, Nodejs, Express, MongoDB, etc.
                   </p>
                 <p>I am proficient in the use of modern technologies for front-end and back-end development. I am available for hire.</p>
               </div>
